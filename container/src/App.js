fdc7560d2b3c9e705b29d95e05d78a7460246a01
Santiago Nuñez <tau150@hotmail.com>
2021-01-16 17:14:19 -0300

    header

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import MarketingApp from './components/MarketingApp'
-
+import { BrowserRouter } from 'react-router-dom';
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
   return (
-    <div>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   )
 
 }
